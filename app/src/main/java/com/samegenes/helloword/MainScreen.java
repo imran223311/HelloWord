868fbc781b754d6db124e97bdca461776cfa1adc
imran223311 <Aissara_P@cmu.ac.th>
2024-02-28 17:40:55 +0700

    "Update GameScreen and connect layout almost finish"

--- app/src/main/java/com/samegenes/helloword/MainScreen.java
+++ app/src/main/java/com/samegenes/helloword/MainScreen.java
@@ -2,7 +2,9 @@ package com.samegenes.helloword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+    }
+
+    public void goGameScreen(View view){
+
+        Intent gameScreen = new Intent(this, GameScreen.class);
+        startActivity(gameScreen);
+    }
+
+    public void goSettingScreen(View view){
+
+        Intent settingScreen = new Intent(this, SettingScreen.class);
+        startActivity(settingScreen);
     }
 }
